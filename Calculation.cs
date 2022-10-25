10f34b92d5a3bf77011fb25a1942669540ecc091
Edward Vaudin <edward@vaudin.net>
2022-10-25 09:11:52 +0100

    Imported Calculator project to Console Review
    I was having issues with importing the project reference set up that Microsoft's tutorial has you do, so I merged the two projects.

---
+++ Calculation.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorLibrary
+{
+    public class Calculation
+    {
+        protected double num1 = 0;
+        protected double num2 = 0;
+        public double Result { get; protected set; } = 0;
+        protected string op = "";
+
+        public Calculation(double num1, double num2, double result, string op)
+        {
+            this.num1 = num1;
+            this.num2 = num2;
+            this.Result = result;
+            this.op = PrettifyOperator(op);
+        }
+
+        public override string ToString()
+        {
+            return $"{num1} {op} {num2} = {Result}\n";
+        }
+
+        private static string PrettifyOperator(string op)
+        {
+            switch (op)
+            {
+                case "a":
+                    op = "+";
+                    break;
+                case "s":
+                    op = "-";
+                    break;
+                case "m":
+                    op = "*";
+                    break;
+                case "d":
+                    op = "/";
+                    break;
+                case "p":
+                    op = "^";
+                    break;
+                default:
+                    break;
+            }
+            return op;
+        }
+    }
+}
---
+++ TrigCalculation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorLibrary
+{
+    public class TrigCalculation : Calculation
+    {
+        public TrigCalculation(double num1, double num2, double result, string op) : base(num1, num2, result, op)
+        {
+            this.num1 = num1;
+            this.num2 = num2;
+            this.Result = result;
+            this.op = op;
+        }
+
+        public override string ToString()
+        {
+            return $"{op}({num1}) = {Result}\n";
+        }
+    }
+}
